d595665af8f244b77a2f7f8002343b31ad11740e
ConnerIBlair <connerblair072006@gmail.com>
2026-03-19 20:03:34 -0700

    Nuxt start

---
+++ app/app.vue
@@ -0,0 +1,7 @@
+<template>
+  <sec class="flex flex-row justify-between gap-4 max-w-6xl mx-auto">
+    <Bucket title="hi"/>
+    <Bucket />
+    <Bucket />
+  </sec>
+</template>
---
+++ app/components/Bucket.vue
@@ -0,0 +1,23 @@
+<template>
+    <article class=" flex-1 p-6 rounded-xl" :class="className">
+    <div class="font-bold text-xl">{{title}}</div>
+    <div>
+    
+        <slot/>
+        
+    </div>
+    </article>
+</template>
+<script setup>
+    const props = defineProps({
+        title:{
+            default: 'Title'
+        },
+        body:{
+            default: ' '
+            },
+        className:{
+            default:'bg-gray-800'
+        }
+    })
+</script>
