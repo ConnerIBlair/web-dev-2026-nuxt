3c8e75da0f0f846c270a2dc698bab529521da8ae
Conner <conner.blair072006@gmail.com>
2026-04-09 19:22:59 -0700

    Update

--- app/app.vue
+++ app/app.vue
@@ -1,7 +1,9 @@
 <template>
-  <sec class="flex flex-row justify-between gap-4 max-w-6xl mx-auto">
-    <Bucket title="hi"/>
-    <Bucket />
-    <Bucket />
-  </sec>
+<UApp>
+  <TopNav/>
+  <MainNav/>
+  <Hero/>
+  <Cta/>
+  <Footer/>
+</UApp>
 </template>
